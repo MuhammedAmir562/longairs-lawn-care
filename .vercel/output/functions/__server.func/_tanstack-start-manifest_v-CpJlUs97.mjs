ad030b7ce2f1a1cce5b354748ed29df05aa4243d
Muhammed <fkddku@gmail.com>
2026-06-01 16:15:27 +0100

    fix: correctly ignore vercel output

--- .vercel/output/functions/__server.func/_tanstack-start-manifest_v-CpJlUs97.mjs
+++
@@ -1,4 +0,0 @@
-const tsrStartManifest = () => ({ routes: { __root__: { filePath: "D:/Muhammed/longairs-lawn-care/src/routes/__root.tsx", children: ["/", "/sitemap.xml"], preloads: ["/assets/index-B28GV9n-.js"], scripts: [{ attrs: { type: "module", async: true, src: "/assets/index-B28GV9n-.js" } }] }, "/": { filePath: "D:/Muhammed/longairs-lawn-care/src/routes/index.tsx", children: void 0, preloads: ["/assets/index-BiSWIxwh.js"] } } });
-export {
-  tsrStartManifest
-};
